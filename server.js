807105e4785360a60136f06050045b3af52f0c40
yair man <yairman2429@gmail.com>
2023-01-29 18:02:40 +0200

    הוספת הווב סוקט

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const map_router = require('./src/routs/map')
+const ws = require('ws');
+const socket_server = new ws.Server({noServer: true})
+
+socket_server.on('connection', ()=>{
+    console.log('conn')
+})
 
 const app = express()
 const port = 3020
 
 app.use('/map', map_router)
 
-app.listen(port, function(){
+const server = app.listen(port, function(){
     console.log("express start")
+})
+
+server.on('upgrade', (request, socket, head) => {
+    socket_server.handleUpgrade(request, socket, head, socket => {
+        socket_server.emit('connection', socket, request);
+    });
 })
